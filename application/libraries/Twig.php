4e79cce0848c8591049cc847cdc0097ee5322d68
Josue Aponte <josueaponte7@gmail.com>
2017-07-09 17:47:31 -0400

    Platilla con twig, elementos form

--- application/libraries/Twig.php
+++ application/libraries/Twig.php
@@ -18,12 +18,9 @@ class Twig
 {
 	private $config = [];
 
-	private $functions_asis = [
-		'base_url', 'site_url'
-	];
-	private $functions_safe = [
-		'form_open', 'form_close', 'form_error', 'set_value', 'form_hidden'
-	];
+	private $functions_asis = ['base_url', 'site_url', 'public_url', 'menu', 'submenu', 'projectname', 'resources_admin', 'resources', 'lang', 'getname', 'prueba', 'languajes','multi_languaje'];
+	private $functions_safe = ['form_open', 'form_close', 'form_error','form_input' , 'form_hidden', 'form_password', 'form_button', 'set_value', 'form_hidden', 'img', 'heading', 'link_tag', 'script_tag', 'validation_errors', 'form_submit', 'form_label'];
+	private $filters_asis   = ['price', 'dateformat'];
 
 	/**
 	 * @var bool Whether functions are added or not
@@ -42,7 +39,6 @@ class Twig
 
 	public function __construct($params = [])
 	{
-		// default config
 		$this->ci =& get_instance();
 		$this->ci->load->library('session');
 
@@ -50,20 +46,14 @@ class Twig
 
 		$this->config = array_merge($this->config, $params);
 
-		if (isset($params['functions']))
-		{
-			$this->functions_asis =
-				array_unique(
-					array_merge($this->functions_asis, $params['functions'])
-				);
+		if (isset($params['functions'])){
+			$this->functions_asis = array_unique(array_merge($this->functions_asis, $params['functions']));
 		}
-		if (isset($params['functions_safe']))
-		{
-			$this->functions_safe =
-				array_unique(
-					array_merge($this->functions_safe, $params['functions_safe'])
-				);
+		if (isset($params['functions_safe'])){
+			$this->functions_safe = array_unique(array_merge($this->functions_safe, $params['functions_safe']));
 		}
+
+		$this->addGlobal("session", $this->ci->session);
 	}
 
 	protected function resetTwig()
@@ -75,33 +65,23 @@ class Twig
 	protected function createTwig()
 	{
 		// $this->twig is singleton
-		if ($this->twig !== null)
-		{
+		if ($this->twig !== null){
 			return;
 		}
 
-		if (ENVIRONMENT === 'production')
-		{
+		if (ENVIRONMENT === 'production') {
 			$debug = FALSE;
-		}
-		else
-		{
+		}else{
 			$debug = TRUE;
 		}
 
-		if ($this->loader === null)
-		{
+		if ($this->loader === null) {
 			$this->loader = new \Twig_Loader_Filesystem($this->config['paths']);
 		}
 
-		$twig = new \Twig_Environment($this->loader, [
-			'cache'      => $this->config['cache'],
-			'debug'      => $debug,
-			'autoescape' => TRUE,
-		]);
+		$twig = new \Twig_Environment($this->loader, ['cache' => $this->config['cache'], 'debug' => $debug, 'autoescape' => TRUE,]);
 
-		if ($debug)
-		{
+		if ($debug){
 			$twig->addExtension(new \Twig_Extension_Debug());
 		}
 
@@ -134,6 +114,7 @@ class Twig
 	public function display($view, $params = [])
 	{
 		$CI =& get_instance();
+
 		$CI->output->set_output($this->render($view, $params));
 	}
 
@@ -146,62 +127,59 @@ class Twig
 	 */
 	public function render($view, $params = [])
 	{
+
 		$this->createTwig();
 		// We call addFunctions() here, because we must call addFunctions()
 		// after loading CodeIgniter functions in a controller.
+
 		$this->addFunctions();
+
+		$this->addFilters();
+
+		$this->filter();
+
+		$this->functionname();
 
 		$view = $view . '.twig';
 		return $this->twig->render($view, $params);
+
+	}
+
+	protected function addFilters()
+	{
+		foreach ($this->filters_asis as $filter) {
+			if (function_exists($filter)) {
+				$this->twig->addFilter(new \Twig_SimpleFilter($filter, $filter ));
+			}
+		}
+
 	}
 
 	protected function addFunctions()
 	{
 		// Runs only once
-		if ($this->functions_added)
-		{
+		if ($this->functions_added){
 			return;
 		}
 
 		// as is functions
-		foreach ($this->functions_asis as $function)
-		{
-			if (function_exists($function))
-			{
-				$this->twig->addFunction(
-					new \Twig_SimpleFunction(
-						$function,
-						$function
-					)
-				);
+		foreach ($this->functions_asis as $function){
+			if (function_exists($function)){
+				$this->twig->addFunction(new \Twig_SimpleFunction($function, $function));
 			}
 		}
 
 		// safe functions
-		foreach ($this->functions_safe as $function)
-		{
-			if (function_exists($function))
-			{
-				$this->twig->addFunction(
-					new \Twig_SimpleFunction(
-						$function,
-						$function,
-						['is_safe' => ['html']]
-					)
-				);
+		foreach ($this->functions_safe as $function){
+			if (function_exists($function)){
+				$this->twig->addFunction(new \Twig_SimpleFunction($function, $function, ['is_safe' => ['html']]));
 			}
 		}
 
 		// customized functions
-		if (function_exists('anchor'))
-		{
-			$this->twig->addFunction(
-				new \Twig_SimpleFunction(
-					'anchor',
-					[$this, 'safe_anchor'],
-					['is_safe' => ['html']]
-				)
-			);
+		if (function_exists('anchor')){
+
+			$this->twig->addFunction(new \Twig_SimpleFunction('anchor', [$this, 'safe_anchor'], ['is_safe' => ['html']]));
 		}
 
 		$this->functions_added = TRUE;
@@ -219,8 +197,7 @@ class Twig
 		$title = html_escape($title);
 
 		$new_attr = [];
-		foreach ($attributes as $key => $val)
-		{
+		foreach ($attributes as $key => $val){
 			$new_attr[html_escape($key)] = html_escape($val);
 		}
 
@@ -234,5 +211,21 @@ class Twig
 	{
 		$this->createTwig();
 		return $this->twig;
+	}
+
+	public function filter()
+	{
+		$filter = new Twig_SimpleFilter('anonima', function ($string, $otro='') {
+			return strtoupper($string).' Otro: '.$otro;
+		});
+		$this->twig->addFilter($filter);
+	}
+
+	public function functionname()
+	{
+		$function = new Twig_SimpleFunction('saludo', function ($name) {
+			return 'Hola '.$name;
+		});
+		$this->twig->addFunction($function);
 	}
 }
